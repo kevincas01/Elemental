902b746e88dd0131aa7809d6452f9b0be6dd8895
Kevin Castro <kcapupp@princeton.edu>
2025-02-10 20:23:31 -0500

    Layout, including header

--- elemental/app/layout.tsx
+++ elemental/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./ReusableComponents/Header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen text-lightTextContrast bg-lightBg dark:text-darkTextContrast dark:bg-darkBg`}
       >
-        {children}
+        <Header />
+        <div className="pt-[75px]">{children} </div>
       </body>
     </html>
   );
