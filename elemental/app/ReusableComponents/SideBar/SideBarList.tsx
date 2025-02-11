3ebf48b8c2a81b9480afaecc7ad8fa4c46118925
Kevin Castro <kcapupp@princeton.edu>
2025-02-11 11:27:24 -0500

    Adding key props

--- elemental/app/ReusableComponents/SideBar/SideBarList.tsx
+++ elemental/app/ReusableComponents/SideBar/SideBarList.tsx
@@ -2,16 +2,19 @@ import { listItem } from "@/app/constants/componentsList";
 import Link from "next/link";
 import React from "react";
 
-
 interface SideBarListProps {
   list: listItem[];
 }
 const SideBarList = ({ list }: SideBarListProps) => {
   return (
     <div className="flex flex-col gap-1 ">
-      {list.map((section) => (
-        <Link href={section.url} className="hover:bg-lightSecondaryBg dark:hover:bg-darkSecondaryBg py-2 px-3">
-          <p>{section.name}</p>{" "}
+      {list.map((listItem) => (
+        <Link
+          key={listItem.name}
+          href={listItem.url}
+          className="hover:bg-lightSecondaryBg dark:hover:bg-darkSecondaryBg py-2 px-3"
+        >
+          <p>{listItem.name}</p>{" "}
         </Link>
       ))}
     </div>
