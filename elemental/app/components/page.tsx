f87a3d0b72d2e735401a5a265ffa549c7a232596
Kevin Castro <kcapupp@princeton.edu>
2025-02-10 22:00:16 -0500

    components page

---
+++ elemental/app/components/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { componentsList, sectionsList } from "../constants/componentsList";
+import ComponentGallery from "../ReusableComponents/ComponentGallery";
+const page = () => {
+  return (
+    <div className="">
+      <h1 className="text-5xl">All Components</h1>
+      <p>
+        All components are built with React, Tailwind CSS, and vanilla
+        JavaScript or CSS, ensuring a lightweight and simple approach. We focus
+        on delivering code that doesn't rely on any extra packages or libraries,
+        so you can easily integrate components into your project without
+        additional dependencies. Everything is designed to be intuitive and
+        customizable, keeping up with the latest versions of React and Tailwind.
+      </p>
+      <h2 className="text-2xl">Sections</h2>
+      <ComponentGallery list={sectionsList}/>
+      <h2 className="text-2xl">
+        Components
+      </h2>
+      <ComponentGallery list={componentsList}/>
+    </div>
+  );
+};
+
+export default page;
