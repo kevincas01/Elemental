699153dbb461e8552b7952d2b088f259b021d831
Kevin Castro <kcapupp@princeton.edu>
2025-02-12 12:20:01 -0500

    Lift button component

---
+++ elemental/app/ReusableComponents/ButtonComponents/LiftButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LiftButton = () => {
+  return (
+    <a
+      className="block border-2 border-black w-full p-5 text-center font-semibold uppercase text-primary bg-lightBg transition-all duration-200 transform hover:translate-x-[10px] hover:-translate-y-[10px] hover:[box-shadow:-10px_10px_0_#000] "
+      href="#"
+    >
+      Hover Me!
+    </a>
+  );
+};
+
+export default LiftButton;
