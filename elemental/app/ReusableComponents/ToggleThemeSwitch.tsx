7509a0745019c2722d25410b6eca1a5366946d51
Kevin Castro <kcapupp@princeton.edu>
2025-02-15 17:58:03 -0500

    rm nn

--- elemental/app/ReusableComponents/ToggleThemeSwitch.tsx
+++ elemental/app/ReusableComponents/ToggleThemeSwitch.tsx
@@ -37,10 +37,10 @@ const ToggleThemeSwitch = () => {
         name="darkMode"
         checked={isDarkMode}
         onChange={toggleTheme}
-        className="settings-checkbox absolute w-full h-full appearance-none rounded-full mix-blend-difference bg-lightBg  border cursor-pointer transition-colors duration-400"
+        className=" absolute w-full h-full rounded-full mix-blend-difference bg-lightBg border cursor-pointer transition-colors duration-400"
       />
       <span
-        className={`absolute  top-0 left-0 h-full aspect-square rounded-full bg-lightBg dark:bg-darkBg transition-transform duration-200 ${
+        className={`absolute top-0 left-0 h-full aspect-square rounded-full bg-lightBg dark:bg-darkBg transition-transform duration-200 ${
           isDarkMode ? "translate-x-full " : ""
         }`}
       ></span>
