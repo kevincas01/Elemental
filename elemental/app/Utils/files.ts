9c39860df77b3761bb5732f8437d6079cad3f7b2
Kevin Castro <kcapupp@princeton.edu>
2025-02-25 19:33:09 -0500

    test files read solve

--- elemental/app/Utils/files.ts
+++ elemental/app/Utils/files.ts
@@ -1,14 +1,16 @@
-import { join } from "path";
+import { join, resolve } from "path";
 import { readFileSync } from "fs";
 export const getFileContent = (filePath: string, language: string): string => {
   try {
     // Determine the base path based on language
     const basePath =
-      language === "css" ? "./app/Styles/UIGallery/" : "./app/Components/UIGallery/";
+      language === "css"
+        ? "./app/Styles/UIGallery/"
+        : "./app/Components/UIGallery/";
 
-    const fullPath = join(process.cwd(), basePath+"/"+ filePath);
+    const fullPath = join(process.cwd(), basePath + filePath);
 
-    return readFileSync(fullPath, "utf-8");
+    return readFileSync(resolve(fullPath), "utf-8");
   } catch (error) {
     console.error(`Error reading file (${filePath}):`, error);
     return `// Error loading file content: ${filePath}`;
