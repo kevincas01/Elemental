3d8e16601822035b1eb0fe4afb9966c8552b63bc
Kevin Castro <kcapupp@princeton.edu>
2025-02-28 21:44:12 -0500

    Card icon size change

--- elemental/app/Components/Card.tsx
+++ elemental/app/Components/Card.tsx
@@ -9,7 +9,7 @@ const Card = ({ title, body, icon }: CardProps) => {
   return (
     <div className="border rounded-lg p-5 hover:-translate-y-2 hover:shadow-darkCard dark:hover:shadow-lightCard dark:bg-darkBg dark:border-darkBgBorder transition-all duration-300">
       <h2 className="text-2xl">
-        {title} {icon && <span className="text-primary">{icon}</span>}
+        {title} {icon && <span className="text-primary text-4xl">{icon}</span>}
       </h2>
       <p className="text-grayText">{body}</p>
     </div>
