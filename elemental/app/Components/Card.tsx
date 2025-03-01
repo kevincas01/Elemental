eade92b6f54f4874441a48dbbd11d95510ad23e1
Kevin Castro <kcapupp@princeton.edu>
2025-02-28 21:37:58 -0500

    Add icon prop to card component

--- elemental/app/Components/Card.tsx
+++ elemental/app/Components/Card.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 interface CardProps {
   title: string;
   body: string;
+  icon?: ReactNode;
 }
-const Card = ({ title, body }: CardProps) => {
+const Card = ({ title, body, icon }: CardProps) => {
   return (
-    <div className="border rounded-lg p-5 hover:shadow-darkCard dark:hover:shadow-lightCard dark:bg-darkBg dark:border-darkBgBorder">
-      <h2 className="text-xl">{title}</h2>
+    <div className="border rounded-lg p-5 hover:-translate-y-2 hover:shadow-darkCard dark:hover:shadow-lightCard dark:bg-darkBg dark:border-darkBgBorder transition-all duration-300">
+      <h2 className="text-2xl">
+        {title} {icon && <span className="text-primary">{icon}</span>}
+      </h2>
       <p className="text-grayText">{body}</p>
     </div>
   );
