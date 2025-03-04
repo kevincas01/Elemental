d52d5cc787cd340dc3237e4bd3aa5a8bab5f0270
Kevin Castro <kcapupp@princeton.edu>
2025-03-03 19:52:09 -0500

    Change timing and delays:link expand circle component

--- elemental/public/UIGallery/Components/OtherComponents/LinkExpandCircle.tsx
+++ elemental/public/UIGallery/Components/OtherComponents/LinkExpandCircle.tsx
@@ -7,17 +7,17 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 const LinkExpandCircle = () => {
   return (
     <div className="relative group h-[90px] hover:h-[200px] aspect-square transition-all duration-500 rounded-full flex flex-col justify-center items-center text-3xl ">
-      <button className="p-4 group-hover:opacity-0 transition-all duration-300 group-hover:delay-100 delay-200 absolute w-[90px] h-[90px] bg-white rounded-full  flex items-center justify-center ease-in-out">
+      <button className="p-4 group-hover:opacity-0 transition-all duration-300 group-hover:delay-0 delay-200 absolute w-[90px] h-[90px] bg-white rounded-full  flex items-center justify-center ease-in-out">
         <span className="text-black">
           <ShareIcon fontSize="inherit" />
         </span>
       </button>
 
       <div
-        className="absolute h-[90px] min-h-0 group-hover:h-[200px] aspect-square overflow-hidden rounded-full flex flex-col gap-0 group-hover:gap-3 justify-center items-center"
-      style={{transition:"height 500ms, gap 500ms ease-in"}}
+        className="absolute h-[90px] min-h-0 group-hover:h-[200px] aspect-square overflow-hidden rounded-full flex flex-col gap-0 group-hover:gap-3 justify-center items-center delay-0"
+        style={{ transition: "height 500ms, gap 600ms ease-in " }}
       >
-        <div className="flex gap-0 group-hover:gap-3 transition-all duration-500 ease-in">
+        <div className="flex gap-0 group-hover:gap-3 transition-all duration-300 ease-in group-hover:delay-300">
           <button className="relative box-border cursor-pointer w-[90px] pl-4 pt-4 aspect-square  bg-white rounded-tl-full  transition-all duration-600 ease-in-out origin-bottom-right scale-0 group-hover:scale-100 hover:scale-110 hover:bg-[#cc39a4] text-[#cc39a4] hover:text-white">
             <InstagramIcon fontSize="inherit" />
           </button>
@@ -27,7 +27,7 @@ const LinkExpandCircle = () => {
           </button>
         </div>
 
-        <div className="flex gap-0 group-hover:gap-3 transition-all duration-500 ease-in">
+        <div className="flex gap-0 group-hover:gap-3 transition-all duration-300 ease-in group-hover:delay-300">
           <button className="box-border cursor-pointer w-[90px] pl-4 pb-4 aspect-square  bg-white rounded-bl-full transition-all duration-600 ease-in-out origin-top-right scale-0 group-hover:scale-100 hover:scale-110 hover:bg-black text-black hover:text-white">
             <GitHubIcon fontSize="inherit" />
           </button>
