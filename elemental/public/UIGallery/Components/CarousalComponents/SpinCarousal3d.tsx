fa4ad4679a26f177441fff83e2338a0152487b46
Kevin Castro <kcapupp@princeton.edu>
2025-03-03 22:22:02 -0500

    Spin carousal3d public files

---
+++ elemental/public/UIGallery/Components/CarousalComponents/SpinCarousal3d.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "@/app/Styles/UIGallery/SpinCarousal3d.css";
+
+const SpinCarousal3d = () => {
+
+  return (
+    <div className="card-3d">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default SpinCarousal3d;
