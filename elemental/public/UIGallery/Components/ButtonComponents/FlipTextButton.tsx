f8ca3287fe5745908fec6ae541bd62f510848e99
Kevin Castro <kcapupp@princeton.edu>
2025-03-17 22:34:59 -0400

    Flip text button public files

---
+++ elemental/public/UIGallery/Components/ButtonComponents/FlipTextButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "@/app/Styles/UIGallery/FlipTextButton.css";
+
+const FlipTextButton = () => {
+  return (
+    <div className="flip-text-button">
+      <div className="flip-text-box">H</div>
+      <div className="flip-text-box">O</div>
+      <div className="flip-text-box">V</div>
+      <div className="flip-text-box">E</div>
+      <div className="flip-text-box">R</div>
+    </div>
+  );
+};
+
+export default FlipTextButton;
