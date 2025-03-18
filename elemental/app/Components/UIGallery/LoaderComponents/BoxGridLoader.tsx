22c3b8439a4b9150d2af6070fe1713aff9347cb2
Kevin Castro <kcapupp@princeton.edu>
2025-03-18 13:25:09 -0400

    Box Grid Loader component

---
+++ elemental/app/Components/UIGallery/LoaderComponents/BoxGridLoader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "@/app/Styles/UIGallery/BoxGridLoader.css";
+const BoxGridLoader = () => {
+  return (
+    <div className="loader">
+      <div className="box1"></div>
+      <div className="box2"></div>
+      <div className="box3"></div>
+    </div>
+  );
+};
+
+export default BoxGridLoader;
