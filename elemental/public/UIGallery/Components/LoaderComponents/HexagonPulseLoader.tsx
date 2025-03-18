f73abf58719440500556668b972770076e94b8d7
Kevin Castro <kcapupp@princeton.edu>
2025-03-18 12:35:57 -0400

    Hexagon pulse loader public files

---
+++ elemental/public/UIGallery/Components/LoaderComponents/HexagonPulseLoader.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "@/app/Styles/UIGallery/HexagonPulseLoader.css";
+const HexagonPulseLoader = () => {
+  return (
+    <div className="hexagon-loader-container">
+      <div className="hexagon-row">
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring3"></div>
+      </div>
+      <div className="hexagon-row">
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring3"></div>
+      </div>
+      <div className="hexagon-row">
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring1"></div>
+        <div className="hexagon ring1"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring3"></div>
+      </div>
+      <div className="hexagon-row">
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring1"></div>
+        <div className="hexagon center"></div>
+        <div className="hexagon ring1"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring3"></div>
+      </div>
+      <div className="hexagon-row">
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring1"></div>
+        <div className="hexagon ring1"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring3"></div>
+      </div>
+      <div className="hexagon-row">
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring2"></div>
+        <div className="hexagon ring3"></div>
+      </div>
+      <div className="hexagon-row">
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring3"></div>
+        <div className="hexagon ring3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default HexagonPulseLoader;
