7056956186006b61a54698ec2756cef9865dbd98
Kevin Castro <kcapupp@princeton.edu>
2025-04-07 21:34:46 -0400

    Label shift input public files

---
+++ elemental/public/UIGallery/Components/InputComponents/LabelShiftInput.tsx
@@ -0,0 +1,50 @@
+import React, { ChangeEvent, FocusEvent, useRef, useState } from "react";
+
+const LabelShiftInput = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [hasValue, setHasValue] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+    setIsFocused(false);
+    setHasValue(e.target.value !== "");
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setHasValue(e.target.value !== "");
+  };
+
+  const handleLabelClick = () => {
+    inputRef.current?.focus();
+  };
+
+  const labelClasses =
+    "cursor-text text-lg absolute left-4 transition-all duration-300 bg-white px-1 -translate-y-1/2" +
+    (isFocused || hasValue ? " top-0  text-sm" : " top-1/2 ");
+
+  return (
+    <div className="flex flex-col relative mt-8">
+      <label
+        htmlFor="username"
+        onClick={handleLabelClick}
+        className={labelClasses}
+      >
+        Username
+      </label>
+      <input
+        id="username"
+        ref={inputRef}
+        type="text"
+        name="username"
+        required
+        className="border-2 border-red-300 focus:border-main focus:outline-none p-2 rounded-md"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default LabelShiftInput;
