59d46042eb30557551f9610567370057086ab3f3
Kevin Castro <kcapupp@princeton.edu>
2025-04-07 21:32:31 -0400

    Card icon bg

--- elemental/app/Components/Card.tsx
+++ elemental/app/Components/Card.tsx
@@ -7,11 +7,12 @@ interface CardProps {
 }
 const Card = ({ title, body, icon }: CardProps) => {
   return (
-    <div className="border rounded-lg p-5 hover:-translate-y-2 hover:scale-105 hover:shadow-darkCard dark:hover:shadow-lightCard dark:bg-darkBg dark:border-darkBgBorder transition-all duration-300">
-      <h2 className="text-2xl">
-        {title} {icon && <span className="text-primary text-4xl">{icon}</span>}
-      </h2>
+    <div className="relative border rounded-lg p-5 hover:-translate-y-2 hover:scale-102 hover:shadow-darkCard dark:hover:shadow-lightCard dark:bg-darkBg dark:border-darkBgBorder transition-all duration-300 overflow-hidden">
+      <h2 className="text-2xl">{title}</h2>
       <p className="text-grayText">{body}</p>
+      <span className="text-primary opacity-15 absolute scale-[5] top-1/2 left-3/4 -translate-x-3/4 -translate-y-1/2 -rotate-20 z-0 text-5xl ">
+        {icon}
+      </span>
     </div>
   );
 };
