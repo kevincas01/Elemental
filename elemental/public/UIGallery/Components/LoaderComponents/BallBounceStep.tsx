0be5a9efb9ec0111c2a74b4fbd80206bc7e58af8
Kevin Castro <kcapupp@princeton.edu>
2025-04-08 22:36:58 -0400

    Public files ball bounce step component

---
+++ elemental/public/UIGallery/Components/LoaderComponents/BallBounceStep.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "@/app/Styles/UIGallery/BallBounceStep.css";
+
+const BallBounceStep = () => {
+  return (
+    <div className="step-loader">
+      <div className="step-ball"></div>
+    </div>
+  );
+};
+
+export default BallBounceStep;
