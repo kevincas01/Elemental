a3151e7eec60cff448e2d856db32ae9b388d276f
Kevin Castro <kcapupp@princeton.edu>
2025-04-22 22:52:53 -0400

    Change props for file selection

--- elemental/app/Components/UIGallery/FileSelection.tsx
+++ elemental/app/Components/UIGallery/FileSelection.tsx
@@ -1,25 +1,25 @@
 import { File, FileGroup } from "@/app/Types/files";
 import React, { Dispatch, SetStateAction } from "react";
 interface FileSelectionProps {
-  selectedGroupIndex: number;
-  selectedFile: File | null;
-  setSelectedFile: Dispatch<SetStateAction<File | null>>;
+  selectedFileGroupIndex: number;
+  selectedFileIndex: number;
+  setSelectedFileIndex: Dispatch<SetStateAction<number>>;
   fileGroups: FileGroup[];
 }
 const FileSelection = ({
-  selectedGroupIndex,
-  selectedFile,
-  setSelectedFile,
+  selectedFileGroupIndex,
+  selectedFileIndex,
+  setSelectedFileIndex,
   fileGroups,
 }: FileSelectionProps) => {
   return (
     <div className="flex w-full sticky top-0">
-      {fileGroups[selectedGroupIndex].files.map((file) => (
+      {fileGroups[selectedFileGroupIndex].files.map((file, index) => (
         <button
           key={file.path}
-          onClick={() => setSelectedFile(file)}
+          onClick={() => setSelectedFileIndex(index)}
           className={`p-3 ${
-            selectedFile?.path === file.path ? "bg-primary" : "cursor-pointer"
+            selectedFileIndex === index ? "bg-primary" : "cursor-pointer"
           }`}
         >
           {file.title}
